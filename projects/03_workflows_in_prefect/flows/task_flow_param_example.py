fd835c8cd9332a21489fbe8203ee010b4cc7d22e
pengfei liu <liu.pengfei@hotmail.fr>
2025-10-31 09:14:54 +0100

    refact prefect project

---
+++ projects/03_workflows_in_prefect/flows/task_flow_param_example.py
@@ -0,0 +1,62 @@
+from prefect import task, flow
+
+
+@task(
+    name="task1",
+    description="task 1 of my workflow",
+    retries=3,
+    retry_delay_seconds=30,
+    timeout_seconds=120,
+    persist_result=False,
+    log_prints=True,)
+def task1(input_data: str)->str:
+    msg = f"receive data: {input_data}"
+    print(f"The input value is {input_data}")
+    return msg
+
+
+@task(
+    name="task2",
+    description="task 2 of my workflow",
+    retries=3,
+    retry_delay_seconds=30,
+    timeout_seconds=120,
+    persist_result=False,
+    log_prints=True,)
+def task2():
+    print(f"Running task 2")
+
+
+@task(
+    name="task3",
+    description="task 3 of my workflow",
+    retries=3,
+    retry_delay_seconds=30,
+    timeout_seconds=120,
+    persist_result=False,
+    log_prints=True,)
+def task3():
+    print(f"Running task 3")
+
+
+@flow(
+    name="my_workflow",
+    description="this a test workflow",
+    version="1.0.0",
+    retries=2,
+    retry_delay_seconds=60,
+    persist_result=True,
+    log_prints=True,
+    validate_parameters=True,
+    timeout_seconds=600,
+    task_runner=None,  # e.g., ConcurrentTaskRunner
+)
+def my_flow(param1: str, param2: int = 0):
+    r1 =task1(param1)
+    r2 =task2()
+    task3(wait_for=[r1,r2])
+
+
+if __name__ == "__main__":
+    p1 = "some sample data"
+    my_flow(p1)
